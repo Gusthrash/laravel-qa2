7812c33658926a8a2c22be2c6b1d3866b6a7aef2
Gusthrash <gustavo.nosan@gmail.com>
2018-12-09 23:16:28 -0200

    implement validation

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -2,6 +2,7 @@
 namespace App\Http\Controllers;
 use App\Question;
 use Illuminate\Http\Request;
+use  App\Http\Requests\AskQuestionRequest;
 class QuestionsController extends Controller
 {
     /**
@@ -33,9 +34,11 @@ class QuestionsController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(AskQuestionRequest $request)
     {
-        //
+        $request->user()->questions()->create($request->only('title','body'));
+
+        return redirect()->route('questions.index')->with('sucess',"y question has benn submitted");
     }
     /**
      * Display the specified resource.
